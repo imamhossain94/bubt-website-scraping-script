673cd4c34506a17bf6229fd30d37828e6055fd30
imamhossain94 <imamagun94@gmail.com>
2021-10-06 13:05:44 +0600

    feat: ede_all_courses.py added

---
+++ ede_all_courses.py
@@ -0,0 +1,32 @@
+from requests import get, post, Session
+import json
+from bs4 import BeautifulSoup
+import json
+baseURL = 'https://www.bubt.edu.bd'
+
+def getEDECourse(programUrl):
+    finalData = {'data': list()}
+    try:
+        programHTML = get(baseURL+programUrl).text
+        tables = BeautifulSoup(str(programHTML), 'html.parser').find_all('table')[1:]
+        for table in tables:
+            rows = BeautifulSoup(str(table), 'html.parser').find_all('tr')[0:]
+            localData = dict()
+            for row in rows[0:]:
+                cols = row.find_all('td')
+                if len(cols) == 4:
+                    localData = {
+                            'course_code': cols[1].text.strip(),
+                            'course_title': cols[2].text.strip(),
+                            'theory_credit': '',
+                            'lab_credit': '',
+                            'total_credit': cols[3].text.strip(),
+                            'prerequisit': ''
+                    }
+                    finalData['data'].append(localData)
+        print(json.dumps(finalData))
+    except Exception as ex:
+        finalData = {'status': 'failed', 'reason': str(ex)} 
+    return finalData
+
+getEDECourse("/home/course_details/bsc-hons-in-ede")

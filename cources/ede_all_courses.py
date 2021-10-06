eb455190b72e66f7daebe4c38667de37503300c1
imamhossain94 <imamagun94@gmail.com>
2021-10-07 00:08:05 +0600

    feat: ede_all_courses.json added

--- cources/ede_all_courses.py
+++ cources/ede_all_courses.py
@@ -24,7 +24,7 @@ def getEDECourse(programUrl):
                         'prerequisit': ''
                     }
                     finalData['data'].append(localData)
-        # print(json.dumps(finalData))
+        print(json.dumps(finalData))
     except Exception as ex:
         finalData = {'status': 'failed', 'reason': str(ex)} 
     return finalData

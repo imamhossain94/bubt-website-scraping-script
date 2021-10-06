10dfc44c0fd61245a6dccffc36e48e1ecb517bdb
imamhossain94 <imamagun94@gmail.com>
2021-10-07 00:06:56 +0600

    feat: eco_all_courses.json added

--- cources/eco_all_courses.py
+++ cources/eco_all_courses.py
@@ -25,7 +25,7 @@ def getECOCourse(programUrl):
                         'prerequisit': ''
                     }
                     finalData['data'].append(localData)
-        # print(json.dumps(finalData))
+        print(json.dumps(finalData))
     except Exception as ex:
         finalData = {'status': 'failed', 'reason': str(ex)} 
     return finalData

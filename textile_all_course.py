65a4b07e3196c0a28516fbb12edfd06e66556921
imamhossain94 <imamagun94@gmail.com>
2021-10-06 12:30:52 +0600

    feat: textile_all_courses.py added

---
+++ textile_all_course.py
@@ -0,0 +1,34 @@
+from requests import get, post, Session
+import json
+from bs4 import BeautifulSoup
+import json
+baseURL = 'https://www.bubt.edu.bd'
+
+def getTXECourse(programUrl):
+    finalData = {
+        'data': list()
+    }
+    try:
+        programHTML = get(baseURL+programUrl).text
+        tables = BeautifulSoup(str(programHTML), 'html.parser').find_all('table')[0:]
+        for table in tables:
+            rows = BeautifulSoup(str(table), 'html.parser').find_all('tr')[0:]
+            localData = dict()
+            for row in rows[0:]:
+                cols = row.find_all('td')
+                if len(cols) == 3:
+                    localData = {
+                            'course_code': cols[0].text.strip(),
+                            'course_title': cols[1].text.strip(),
+                            'theory_credit': '',
+                            'lab_credit': '',
+                            'total_credit': cols[2].text.strip(),
+                            'prerequisit': ''
+                    }
+                    finalData['data'].append(localData)
+        print(json.dumps(finalData))
+    except Exception as ex:
+        finalData = {'status': 'failed', 'reason': str(ex)} 
+    return finalData
+
+getTXECourse("/home/course_details/bsc-in-textile-engineering")
